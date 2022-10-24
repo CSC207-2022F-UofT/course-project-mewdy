3150a4df233e4923605197c67189d3349a6650a8
tylerkj1209 <tyler.kristjansen@mail.utoronto.ca>
2022-10-24 11:14:19 -0400

    I added a constructor to the DataPoint so it can actually do something.

--- src/main/java/DataPoint.java
+++ src/main/java/DataPoint.java
@@ -1,4 +1,16 @@
+import java.util.Date;
 
 public class DataPoint {
 
+    private double value;
+    private Date date;
+    private String metricName;
+    private String textEntry;
+
+    public DataPoint(double value, Date date, String metricName, String textEntry) {
+        this.value = value;
+        this.date = date;
+        this.metricName = metricName;
+        this.textEntry = textEntry;
+    }
 }
