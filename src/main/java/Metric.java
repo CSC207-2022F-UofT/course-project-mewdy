7d081b1793b563b1325d22b186a7c6bf8846a039
tylerkj1209 <tyler.kristjansen@mail.utoronto.ca>
2022-10-27 11:29:08 -0400

    I added a MetricStorage class and updated some docstrings for methods in our entities.

--- src/main/java/Metric.java
+++ src/main/java/Metric.java
@@ -8,32 +8,48 @@ public class Metric {
     private double lowerBound;
 
     public Metric(String name, ArrayList<DataPoint> dataPointList, double upperBound, double lowerBound) {
+        //this is a Metric constructor that takes in an ArrayList of DataPoints in addition to the other
+        //instance variables
         this.name = name;
         this.dataPointList = dataPointList;
         this.upperBound = upperBound;
         this.lowerBound = lowerBound;
     }
 
+    public Metric(String name, double upperBound, double lowerBound) {
+        //this is an overloaded Metric constructor for when we want to initialize a new empty Metric
+        this.name = name;
+        this.dataPointList = new ArrayList<DataPoint>();
+        this.upperBound = upperBound;
+        this.lowerBound = lowerBound;
+    }
+
     public ArrayList<DataPoint> getDataPoints() {
+        //this is a method for getting the list of DataPoints from a Metric
         return this.dataPointList;
     }
 
     public void addDataPoint(DataPoint entry) {
+        //this is a method for adding a DataPoint to the end of a Metric's dataPointList
         this.dataPointList.add(entry);
     }
     public void popDataPoint() {
+        //this is a method for removing the most recently added DataPoint from the Metric
         this.dataPointList.remove(-1);
     }
 
     public String getName() {
+        //this is a getter method for the name variable from this Metric
         return this.name;
     }
 
     public double getUpperBound() {
+        //this is a getter method for the upperBound variable from this Metric
         return this.upperBound;
     }
 
     public double getLowerBound() {
+        //this is a getter method for the lowerBound variable from this Metric
         return this.lowerBound;
     }
 }
