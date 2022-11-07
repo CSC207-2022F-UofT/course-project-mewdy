8c2a167f13e7e6531ff7097278434e1384e30120
tylerkj1209 <tyler.kristjansen@mail.utoronto.ca>
2022-11-07 11:30:11 -0500

    Created DataLogger use case interactor

--- src/main/java/UseCases/DataImportInputBoundary.java
+++ src/main/java/UseCases/DataImportInputBoundary.java
@@ -1,5 +1,33 @@
 package UseCases;
 
+import Entities.DataPoint;
+import Entities.Metric;
+import Entities.MetricStorage;
+
 public class DataImportInputBoundary {
 
+    public static class DataLogger implements DataLoggerInputBoundary {
+        public void logDataPoint(String metricName, double value, String textEntry, MetricStorage storage) throws Exception {
+            //this is the UseCases.DataImportInputBoundary.DataLogger use case interactor which takes a metricName, value for the DataPoint,
+            //a text entry, and the metricStorage object that we are adding to.
+            //this throws an exception whenever the metric name is invalid or whenever the
+            //input value is beyond the range of the metric.
+
+            Metric metric = storage.getMetric(metricName);
+            double upperBound = metric.getUpperBound();
+            double lowerBound = metric.getLowerBound();
+
+            if ((lowerBound <= value) && (upperBound >= value)) {
+                DataPoint newEntry = new DataPoint(value, metricName, textEntry);
+                metric.addDataPoint(newEntry);
+            }
+            else {
+                throw new Exception("Invalid value entered: " + value + " is not a valid value for " + metricName);
+            }
+        }
+    }
+
+    public static interface DataLoggerInputBoundary {
+        void logDataPoint(String metricName, double value, String textEntry, MetricStorage storage) throws Exception;
+    }
 }
