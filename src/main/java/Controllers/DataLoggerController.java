9f17a93527112801b71ffd59fcdd321e4191e232
tylerkj1209 <tyler.kristjansen@mail.utoronto.ca>
2022-11-09 12:23:28 -0500

    DataLoggerInputBoundary added and code edited.

--- src/main/java/Controllers/DataLoggerController.java
+++ src/main/java/Controllers/DataLoggerController.java
@@ -3,7 +3,17 @@ package Controllers;
 import Entities.Metric;
 import Entities.MetricStorage;
 import UseCases.DataLogger;
+import UseCases.DataLoggerInputBoundary;
+
+import javax.xml.crypto.Data;
+
 public class DataLoggerController{
+
+    final DataLoggerInputBoundary dataLogger;
+
+    public DataLoggerController(DataLoggerInputBoundary dataLogger){
+        this.dataLogger = dataLogger;
+    }
     public void logDataPoint(double value, String metricName, String textEntry, MetricStorage storage) throws Exception {
         DataLogger dataLogger = new DataLogger(storage);
         dataLogger.logDataPoint(metricName, value, textEntry);
