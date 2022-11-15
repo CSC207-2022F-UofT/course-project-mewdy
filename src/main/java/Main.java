1f0257383ca983f9e51b2ad79fc10ada572f860d
bensonwu02 <benson.wu@mail.utoronto.ca>
2022-11-15 18:52:10 -0500

    Commit 6, 11-15-22
    - UI components cont.
    - testing start

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ import Controllers.MetricSumController;
 import Entities.MetricStorage;
 import Entities.MetricStorageInterface;
 import Presenters.MetricSumPresenter;
-import Screens.WelcomeScreen;
+import Screens.HomeScreen;
+import Screens.StartScreen;
 import UseCases.MetricSumInputBoundary;
 import UseCases.MetricSumOutputBoundary;
 import UseCases.MetricSummarizer;
@@ -14,7 +15,7 @@ public class Main {
     public static void main(String[] args) {
 
         // Build the main program window
-        JFrame application = new JFrame("Mewdy");
+        JFrame application = new StartScreen();
         CardLayout cardLayout = new CardLayout();
         JPanel screens = new JPanel(cardLayout);
         application.add(screens);
@@ -27,9 +28,7 @@ public class Main {
         MetricSumController metricSumController = new MetricSumController(metricSummarizer);
 
         // Build GUI
-        WelcomeScreen welcomeScreen = new WelcomeScreen();
-        screens.add(welcomeScreen, "Mewdy");
-        cardLayout.show(screens, "Mewdy");
+
         application.pack();
         application.setVisible(true);
 
