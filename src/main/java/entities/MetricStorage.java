1b8a1c19d54a22684bcb7d50597b0b86b663175b
Kyle Yang <kyle.yang@mail.utoronto.ca>
2022-11-20 15:03:03 -0500

    Renamed class to MetricStorage

--- src/main/java/entities/MetricStorage.java
+++ src/main/java/entities/MetricStorage.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Objects;
 
-public class Storage implements MetricStorageInterface{
+public class MetricStorage implements MetricStorageInterface{
 
     private final ArrayList<Metric> METRICLIST;
     private File location;
