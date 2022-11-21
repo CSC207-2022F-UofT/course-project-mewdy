bb7b583a5076076572e6c2b71405506149a751bf
bensonwu02 <benson.wu@mail.utoronto.ca>
2022-11-21 10:18:23 -0500

    deleted metric initialization for testing in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,27 +25,7 @@ public class Main {
         // Create metric storage
         MetricStorageInterface metricStorage = new MetricStorage();
 
-
-        //Create data points *test*
-        ArrayList<DataPoint> dataPoints = new ArrayList<>();
-        dataPoints.add(new DataPoint("2018-01-01 19:34:50", 1.0));
-        dataPoints.add(new DataPoint("2018-01-02 19:34:50", 2.0));
-        dataPoints.add(new DataPoint("2018-01-04 19:34:50", 3.0));
-        dataPoints.add(new DataPoint("2018-01-04 19:34:50", 4.0));
-        dataPoints.add(new DataPoint("2018-01-05 19:34:50", 5.0));
-        dataPoints.add(new DataPoint("2018-01-06 19:34:50", 6.0));
-        dataPoints.add(new DataPoint("2018-01-07 19:34:50", 7.0));
-
-        //Create metrics *test*
-        Metric m1 = new Metric("sleep",dataPoints, 24, 0);
-        Metric m2 = new Metric("empty", 10,0);
-
-        metricStorage.addMetric(m1);
-        metricStorage.addMetric(m2);
-
-
-
-        // Create necessary classess to run program
+        // Create necessary classes to run program
         //Import Use Case
         DataImportPresenter dataImportPresenter = new DataImportPresenter();
         DataImportInputBoundary dataImporter = new DataImporter(metricStorage, dataImportPresenter);
