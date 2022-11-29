8fb15404703ac75a8591ec1797aa3a4b74ec6a16
Sanford Lin <craftedswift@gmail.com>
2022-11-29 01:03:05 -0500

    Added JavaDocs to some of the Presenter packages

--- src/main/java/use_cases/MetricDelOutputBoundary.java
+++
@@ -1,9 +0,0 @@
-package use_cases;
-
-import models.MetricDelResponseModel;
-
-public interface MetricDelOutputBoundary {
-    MetricDelResponseModel prepareSuccessView(MetricDelResponseModel responseModel);
-
-    MetricDelResponseModel prepareMetricDelFail(String error);
-}
